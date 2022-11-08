0e2a3d9e77e1089f8d38e03e6461aaa2b77a85b7
yusfamb07 <yusfamuhammadbakran@gmail.com>
2022-11-08 23:37:00 +0700

    Navbar baru

--- components/MainCode/Navbar.js
+++ components/MainCode/Navbar.js
@@ -10,9 +10,8 @@ import Image from 'next/image';
 export default function Navbar(props) {
     return (
    <>
-   <body>
    <section id="main">
-      <nav>
+      {/* <nav>
         <div className="logo">
           <Image src="/img/logo2.png" width={110} height={110} alt="" />
         </div>
@@ -34,106 +33,236 @@ export default function Navbar(props) {
           <li><a href="#">Contact</a></li>
           <li><a href="#">Download</a></li>
         </ul>
-      </nav>
+      </nav> */}
+      <div className="navbar-area">
+        <div className="main-responsive-nav">
+          <div className="container">
+            <div className="main-responsive-menu">
+              <div className="logo">
+                <a href="index.html">
+                  
+                  <img
+                  src="img/logo2.png"
+                  className="white-logo"
+                  alt="image"
+                />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="main-navbar">
+          <div className="container">
+            <nav className="navbar navbar-expand-md navbar-light">
+              <a className="navbar-brand" href="index.html">
+               
+                <img
+                  src="img/logo2.png"
+                  className="white-logo"
+                  alt="image"
+                />
+              </a>
+              <div
+                className="collapse navbar-collapse mean-menu"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a href="index.html" className="nav-link active">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link ">
+                      Wisata
+                      <i className="bx bx-chevron-down" />
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li className="nav-item ">
+                        <a href="wisata-alam.html" className="nav-link">
+                          Wisata Alam
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="wisata-kuliner.html" className="nav-link">
+                          Wisata Kuliner
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="kuliner-tradisional.html" className="nav-link">
+                          Kuliner Tradisional
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="wisata-adat.html" className="nav-link">
+                          Adat &amp; Budaya
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="rekreasi.html" className="nav-link">
+                          Tempat Rekreasi
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="oleh-oleh.html" className="nav-link">
+                          Oleh - Oleh Khas
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Public Service
+                      <i className="bx bx-chevron-down" />
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li className="nav-item">
+                        <a href="penginapan.html" className="nav-link">
+                          Penginapan
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="travel-agensi.html" className="nav-link">
+                          Travel Agensi
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="pelayanan-publik.html" className="nav-link">
+                          Lokasi Pelayanan Publik
+                        </a>
+                      </li>
+                      <hr style={{ backgroundColor: "#a0a0a0" }} />
+                      <li className="nav-item">
+                        <a href="e-lapor.html" className="nav-link">
+                          E-Lapor
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="e-service.html" className="nav-link">
+                          E-Service
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a href="event.html" className="nav-link">
+                      Event
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="traffic.html" className="nav-link">
+                      Traffic
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="news.html" className="nav-link">
+                      News
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Infographic
+                      <i className="bx bx-chevron-down" />
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li className="nav-item">
+                        <a href="weekly-report.html" className="nav-link">
+                          Weekly Traffic Report
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="monthly-report.html" className="nav-link">
+                          Monthly Traffic Report
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Gallery
+                      <i className="bx bx-chevron-down" />
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li className="nav-item">
+                        <a href="foto-kegiatan.html" className="nav-link">
+                          Foto Kegiatan
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a href="transportation.html" className="nav-link">
+                      Transportation Route
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="contact.html" className="nav-link">
+                      Contact
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">
+                      Download
+                      <i className="bx bx-chevron-down" />
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li className="nav-item">
+                        <a href="#" className="nav-link">
+                          Peraturan Daerah
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="#" className="nav-link">
+                          Perundang- Undangan
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                {/* s */}
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div className="others-option-for-responsive">
+          <div className="container">
+            <div className="dot-menu">
+              <div className="inner">
+                <div className="circle circle-one" />
+                <div className="circle circle-two" />
+                <div className="circle circle-three" />
+              </div>
+            </div>
+            <div className="container">
+              <div className="option-inner">
+                <div className="others-options d-flex align-items-center">
+                  <div className="option-item">
+                    <form className="search-box">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search for.."
+                      />
+                      <button type="submit">
+                        <i className="bx bx-search" />
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <video src="images/video.mp4" muted autoPlay loop></video>
-      {/* <div className="overlay"></div> */}
-              {/* <div className="row">
-                <div className="col-lg-8">
-                  <div className="single-main-news" style={{ borderRadius: 10 }}>
-                    <a href="detail.html">
-                      <img src="img/main-news/main-news-1.jpg" alt="image" />
-                    </a>
-                    <div className="news-content">
-                      
-                      <h3>
-                        <a href="detail.html">
-                          Smart City Bali, Teknologi apa yang dipakai disana?
-                        </a>
-                      </h3>
-                      <span>
-                        <a href="detail.html">Walters</a> / 28 September, 2022
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                  <div
-                    className="single-main-news-inner mb-4"
-                    style={{ borderRadius: 8 }}
-                  >
-                    <a href="detail.html">
-                      <img src="img/main-news/main-news-2.jpg" alt="image" />
-                    </a>
-                    <div className="news-content">
-                      
-                      <h3 style={{ fontSize: 20 }}>
-                        <a href="detail.html">
-                          Jepun Bali, Jaringan Elektronik Policing untuk Bali
-                        </a>
-                      </h3>
-                      <span>28 September, 2022</span>
-                    </div>
-                  </div>
-                  <div
-                    className="single-main-news-inner mb-5"
-                    style={{ borderRadius: 8 }}
-                  >
-                    <a href="detail.html">
-                      <img src="img/main-news/main-news-2.jpg" alt="image" />
-                    </a>
-                    <div className="news-content">
-                      
-                      <h3 style={{ fontSize: 20 }}>
-                        <a href="detail.html">Smart City Bali</a>
-                      </h3>
-                      <span>28 September, 2022</span>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
+     
       
     </section>
 
-    {/* <section id="next">
-      <div className="container">
-        <h2>Next Section</h2>
-        <h3>
-          The gradient blends in with the background color of this section
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel suscipit
-          iste ducimus minus qui error debitis exercitationem laboriosam dicta
-          facilis!
-        </p>
-      </div>
-    </section> */}
+   
 
-   </body>
-   {/* <div className="top-header-area bg-white">
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col-lg-6">
-          <ul className="top-header-social">
-            <li>
-              <i className="bx bx-calendar" />
-              <a href="#">
-                Call Center :<strong> 111</strong>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="col-lg-6">
-          <ul className="top-header-others">
-            <li>
-              <i className="bx bx-calendar" />
-              <a href="#">Tue, Oct 25 2022 | 10:11:00 AM</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div> */}
-  {/* End Top Header Area */}
+   
   
   {/* Start Navbar Area */}
   {/* <div className="navbar-area">
