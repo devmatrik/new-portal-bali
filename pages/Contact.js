7d4ac2956c9ca496a4b832ec45f7324f96a92709
yusfamb07 <yusfamuhammadbakran@gmail.com>
2022-11-11 19:34:20 +0700

    upd

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,8 +1,92 @@
 import React from 'react'
+import BaseLayouts from '../components/MainCode/BaseLayouts'
 
 function Contact() {
   return (
-    <div>Contact</div>
+<>
+  {/* Start Page Banner */}
+  <BaseLayouts>
+  <div className="page-title-area">
+    <div className="container">
+      <div className="page-title-content">
+        <h2>Contact</h2>
+      </div>
+    </div>
+  </div>
+  {/* End Page Banner */}
+  {/* Start Contact Area */}
+  <section className="contact-area ptb-50">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-8">
+          <div className="contact-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27982.0992479593!2d-81.35428553933833!3d28.75650994456714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e76d5129fed6b1%3A0x1a6cd960f325cfcb!2sLake%20Mary%2C%20FL%2032746%2C%20USA!5e0!3m2!1sen!2sbd!4v1602575597158!5m2!1sen!2sbd"></iframe>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <aside className="widget-area">
+            <section className="widget widget_stay_connected">
+              <h3 className="widget-title">Location</h3>
+              <ul className="contact-info">
+                <li>
+                  <span>TMC Bali</span>
+                  <br />
+                  Jl. Gunung Sanghyang No.110, Padangsambian, Kec. Denpasar
+                  Bar., Kota Denpasar, Bali 80117
+                </li>
+                <li>
+                  <span>Email</span>
+                  <br />
+                  smartcitybali@mail.com
+                </li>
+                <li>
+                  <span>Call Center</span>
+                  <br />
+                  111
+                </li>
+              </ul>
+            </section>
+            <br />
+            <section className="widget widget_stay_connected">
+              <h3 className="widget-title">Stay connected</h3>
+              <ul className="stay-connected-list">
+                <li>
+                  <a href="#">
+                    <i className="bx bxl-facebook" />
+                    120,345 Fans
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="twitter">
+                    <i className="bx bxl-twitter" />
+                    25,321 Followers
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="youtube">
+                    <i className="bx bxl-youtube" />
+                    101,545 Subs
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="instagram">
+                    <i className="bx bxl-instagram" />
+                    10,129 Followers
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </aside>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/* End Contact Area */}
+  {/* Start Footer Area */}
+
+  </BaseLayouts>
+
+</>
   )
 }
 
--- pages/Gallery/fotoKegiatan.js
+++ pages/Gallery/fotoKegiatan.js
@@ -1,11 +1,137 @@
 import React from 'react'
 import Image from 'next/image'
 import BaseLayouts from '../../components/MainCode/BaseLayouts'
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function fotoKegiatan() {
   return (
     <>
         <BaseLayouts>
+          <>
+        {/* Start Team Area */}
+      <section className="default-news-area">
+        <div className="container">
+          <div className="tech-news ptb-50">
+            <div class="section-title"> 
+              <h2>Foto Kegiatan</h2>
+            </div>
+            <Carousel>
+              <Carousel.Item>
+              <div className="row">
+                <div className="col-lg-3 col-sm-3 ">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                      <h3>
+                        <a href="detail-wisata.html"></a>
+                      </h3>
+                      <p></p>
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-lg-3 col-sm-3">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-2.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                      <h3>
+                        <a href="detail-wisata.html">
+                          
+                        </a>
+                      </h3>
+                      <p></p>
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-lg-3 col-sm-3">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                      <h3>
+                        <a href="detail-wisata.html">
+                          
+                        </a>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-lg-3 col-sm-3">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                      <h3>
+                        <a href="detail-wisata.html">
+                          
+                        </a>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </Carousel.Item>
+              <Carousel.Item>
+              <div className="row">
+                <div className="col-lg-3 col-sm-3 ">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                     
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-lg-3 col-sm-3">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-2.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                     
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-lg-3 col-sm-3">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                     
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-lg-3 col-sm-3">
+                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                    <a href="detail-wisata.html">
+                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
+                    </a>
+                    <div className="tech-news-content">
+                     
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
+      </section>
+      </>
+
             
         </BaseLayouts>
     </>
