58794b96802c884d332201c52ba50f1dabed34a3
yusfamb07 <yusfamuhammadbakran@gmail.com>
2022-11-13 21:37:39 +0700

    konek data wisata

--- pages/Wisata/wisataAlam.js
+++ pages/Wisata/wisataAlam.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import Image from 'next/image'
 import BaseLayouts from '../../components/MainCode/BaseLayouts'
+import {
+  StorageApi
+} from '../../components/MainCode/MainImport'
 import Carousel from 'react-bootstrap/Carousel';
+import moment from 'moment/moment';
 
 export default function wisataAlam (){
+    const [loading, setLoading] = useState(false)
 
+  const [wisata, setWisata] = useState([])
+  useEffect(() => {
+    getData()
+  }, [loading])
+
+
+  const konten = {
+    title: "Wisata Alam",
+    //LinkApi: "algors/periode_keselamatan",
+  }
+
+  const getData = () => {
+    Promise.resolve(StorageApi.getData("sm_portal/wisata/?jenis_wisata_id=4"))
+      .then(value => {
+        const data = value.data.data
+
+
+        setWisata(data)
+
+      }).catch(error => {
+        setWisata(data)
+
+      })
+  }
   return (
     <>
     <BaseLayouts>
@@ -32,134 +62,77 @@ export default function wisataAlam (){
             </div>
             <Carousel>
               <Carousel.Item>
-              <div className="row">
-                <div className="col-lg-3 col-sm-3 ">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">10 Wisata Terindah di Bali</a>
-                      </h3>
-                      <p>28 September, 2022</p>
+                <div className="row">
+                  {wisata.map(item => {
+                      return(
+                        <>
+                        <div className="col-lg-3 col-sm-3 ">
+                          <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                          <a href={`${item.judul}`}>
+                            <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
+                          </a>
+                          <div className="tech-news-content">
+                            <h3>
+                              <a href="">{item.judul}</a>
+                              <p>{item.sub_judul}</p>
+                            </h3>
+                            <p>{moment(item.ctddate).format("DD MMMM, YYYY ")}</p>
+                          </div>
+                          </div>
+                        </div>
+                        </>
+                      )
+                  })}
+                      
+                
+                  {/* <div className="col-lg-3 col-sm-3">
+                    <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                      <a href="detail-wisata.html">
+                        <img src="/images/tech-news/tech-news-2.jpg" alt="image" />
+                      </a>
+                      <div className="tech-news-content">
+                        <h3>
+                          <a href="detail-wisata.html">
+                            Rekomendasi Kuliner Khas Bali yang Wajib Anda Coba
+                          </a>
+                        </h3>
+                        <p>28 September, 2022</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              
-                <div className="col-lg-3 col-sm-3">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-2.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">
-                          Rekomendasi Kuliner Khas Bali yang Wajib Anda Coba
-                        </a>
-                      </h3>
-                      <p>28 September, 2022</p>
+                
+                  <div className="col-lg-3 col-sm-3">
+                    <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                      <a href="detail-wisata.html">
+                        <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
+                      </a>
+                      <div className="tech-news-content">
+                        <h3>
+                          <a href="detail-wisata.html">
+                            11 Hotel dengan Desain Unik di Bali
+                          </a>
+                        </h3>
+                        <p>28 September, 2022</p>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              
-                <div className="col-lg-3 col-sm-3">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">
-                          11 Hotel dengan Desain Unik di Bali
-                        </a>
-                      </h3>
-                      <p>28 September, 2022</p>
+                  </div> */}
+                
+                  {/* <div className="col-lg-3 col-sm-3">
+                    <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
+                      <a href="detail-wisata.html">
+                        <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
+                      </a>
+                      <div className="tech-news-content">
+                        <h3>
+                          <a href="detail-wisata.html">
+                            5 Wisata Desa Adat di Bali, Kaya Sejarah dan Budaya
+                          </a>
+                        </h3>
+                        <p>28 September, 2022</p>
+                      </div>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
-              
-                <div className="col-lg-3 col-sm-3">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">
-                          5 Wisata Desa Adat di Bali, Kaya Sejarah dan Budaya
-                        </a>
-                      </h3>
-                      <p>28 September, 2022</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              </Carousel.Item>
-              <Carousel.Item>
-              <div className="row">
-                <div className="col-lg-3 col-sm-3 ">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">10 Wisata Terindah di Bali</a>
-                      </h3>
-                      <p>28 September, 2022</p>
-                    </div>
-                  </div>
-                </div>
-              
-                <div className="col-lg-3 col-sm-3">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-2.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">
-                          Rekomendasi Kuliner Khas Bali yang Wajib Anda Coba
-                        </a>
-                      </h3>
-                      <p>28 September, 2022</p>
-                    </div>
-                  </div>
-                </div>
-              
-                <div className="col-lg-3 col-sm-3">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">
-                          11 Hotel dengan Desain Unik di Bali
-                        </a>
-                      </h3>
-                      <p>28 September, 2022</p>
-                    </div>
-                  </div>
-                </div>
-              
-                <div className="col-lg-3 col-sm-3">
-                  <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                    <a href="detail-wisata.html">
-                      <img src="/images/tech-news/tech-news-3.jpg" alt="image" />
-                    </a>
-                    <div className="tech-news-content">
-                      <h3>
-                        <a href="detail-wisata.html">
-                          5 Wisata Desa Adat di Bali, Kaya Sejarah dan Budaya
-                        </a>
-                      </h3>
-                      <p>28 September, 2022</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
               </Carousel.Item>
             </Carousel>
           </div>
