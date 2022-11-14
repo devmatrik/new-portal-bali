76470500f983ca5327c573afebdd5f4c5d2dacdb
yusfamb07 <yusfamuhammadbakran@gmail.com>
2022-11-14 13:22:22 +0700

    add detail data

--- pages/Wisata/wisataAlam.js
+++ pages/Wisata/wisataAlam.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
 import Image from 'next/image'
-import { useSession, getSession } from "next-auth/react"
 import BaseLayouts from '../../components/MainCode/BaseLayouts'
 import {
   StorageApi
 } from '../../components/MainCode/MainImport'
 import Carousel from 'react-bootstrap/Carousel';
 import Slider from "react-slick";
-
-
+import Link from 'next/link';
 import Function from '../../components/Function';
 
 // import { Carousel, Card, Stack, Button } from "react-bootstrap";
@@ -17,16 +15,10 @@ import Function from '../../components/Function';
 import moment from 'moment/moment';
 
 export default function wisataAlam (){
-    
-    const [brandcumb, setBrandChumb] = useState([]);
-    const [subbrandcumb, setSubBrandChumb] = useState([]);
-    const { data: session, status } = useSession()
     const [loading, setLoading] = useState(false)
 
     const [wisata, setWisata] = useState([])
-    
     useEffect(() => {
-      Menubrandchumb()
       getData()
     }, [loading])
 
@@ -34,7 +26,7 @@ export default function wisataAlam (){
      dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 4,
+      slidesToShow: 3,
       slidesToScroll: 1
     };
 
@@ -44,7 +36,7 @@ export default function wisataAlam (){
     }
 
   const getData = () => {
-    Promise.resolve(StorageApi.getData("sm_portal/wisata"))
+    Promise.resolve(StorageApi.getData("sm_portal/wisata?jenis_wisata_id=4"))
       .then(value => {
         const data = value.data.data
         setWisata(data)
@@ -57,12 +49,12 @@ export default function wisataAlam (){
   return (
     <>
     <BaseLayouts>
-
+   
       <section className="default-news-area">
         <div className="container">
           <div className="tech-news ptb-50">
             <div className="section-title"> 
-              <h2><Image className="p-2" src="/images/wisataAlam.svg" width={50} height={50} alt="" /> {session?.page}</h2>
+              <h2><Image className="p-2" src="/images/wisataAlam.svg" width={50} height={50} alt="" /> Wisata Alam</h2>
               {/* <h5 style={{ color: "#ff661f", fontWeight: 600, opacity: "80%" }}>Rekomendasi Wisata Alam di Bali</h5> */}
             </div>
              <Slider {...settings}>
@@ -72,48 +64,30 @@ export default function wisataAlam (){
                     <div className="col-lg-3 col-sm-3 ">
                       <div className="single-tech-news-box" style={{ width: "18rem" }}>
                          <a href="">
-                            <img src="/images/tech-news/tech-news-1.jpg"  alt="image" />
+                            <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
                         </a>
+                      {/* <img className="card-img-top" src="/images/tech-news/tech-news-1.jpg" alt="Card image cap" /> */}
                        <div className="tech-news-content">
                         <h3>
-                          <a href="detail-wisata.html">
+                          {/* <a href="../DetailNews/rowid">
                            {item.judul}
-                          </a>
+                          </a> */}
+                          <Link href={'/isata/' + item.rowid} key={item.rowid}>
+                            {item.judul}
+                          </Link>
+                        
                         </h3>
                         <p>{item.sub_judul}</p>
                         <p>{moment(item.ctddate).format("DD MMMM, YYYY ")}</p>
                       </div>
-                    </div>
+                      </div>
                     </div>
                   </div>
                   )
                 })}
 
                
-            {/* {wisata.map(item => {
-              return(
-                <div className="row">
-                    <>
-                    <div className="col-lg-3 col-sm-3 ">
-                        <div className="single-tech-news-box" style={{ borderRadius: 10 }}>
-                        <a href={`${item.judul}`}>
-                            <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
-                        </a>
-                        <div className="tech-news-content">
-                            <h3>
-                            <a href="">{item.judul}</a>
-                            <p>{item.sub_judul}</p>
-                            </h3>
-                            <p>{moment(item.ctddate).format("DD MMMM, YYYY ")}</p>
-                        </div>
-                        </div>
-                    </div>
-
-                    </>
-                </div>
-                )
-            })}  */}
-           
+  
 
             </Slider>
 
