fed12bf607ee73be22f67b16683289065e0299dd
gani3 <ganialabdul@gmail.com>
2022-11-16 01:57:38 +0700

    news dinamic data

--- pages/News/BreakingNews.js
+++ pages/News/BreakingNews.js
@@ -11,27 +11,27 @@ import Carousel from 'react-grid-carousel'
 
 export default function BreakingNews() {
 
-    const [loading, setLoading] = useState(false)
-    const [news, setNews] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [news, setNews] = useState([])
 
-    useEffect(() => {
-      getData();
-    }, [loading])
+  useEffect(() => {
+    getData();
+  }, [loading])
 
-    const settings = {
-     dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
 
-    const konten = {
-      title: "Breaking News",
-      //LinkApi: "algors/periode_keselamatan",
-    }
+  const konten = {
+    title: "Breaking News",
+    //LinkApi: "algors/periode_keselamatan",
+  }
 
-    const getData = () => {
+  const getData = () => {
     Promise.resolve(StorageApi.getData("sm_master_data/jenis_berita"))
       .then(value => {
         const list = value.data.data
@@ -45,61 +45,58 @@ export default function BreakingNews() {
           .then(value => {
             const data = value.data.data
             setNews(data)
-        }).catch(error => {
+          }).catch(error => {
             setNews([])
-        })
-    }).catch(error => {
-    })
-}
+          })
+      }).catch(error => {
+      })
+  }
 
   return (
     <>
-        <BaseLayouts>
-            <section className="default-news-area">
-            <div className="container">
+      <BaseLayouts>
+        <section className="default-news-area">
+          <div className="container">
             <div className="tech-news ptb-50">
-                <div className="section-title"> 
-                    <h2><Image className="p-2" src="/images/News.png" width={50} height={50} alt="" /> Breaking News</h2>
-                    {/* <h5 style={{ color: "#ff661f", fontWeight: 600, opacity: "80%" }}>Rekomendasi Wisata Alam di Bali</h5> */}
-                </div>
-                <Carousel cols={4} rows={1} gap={20} loop>
-                  {news.map((item, index) => (
-                    <Carousel.Item key={index}>
-                                        <div className="row" key={item.rowid}>
-                    <div className="col-lg-3 col-sm-3 ">
-                      <div className="single-tech-news-box rounded-3" style={{ width: "16rem" }}>
-                         <a href="">
-                          {item.image == 0 ? (<>
-                             <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
-                            </>) :(<>
-                            <img src={item.image} style={{ width: "450px", height:"350px" }}/>
+              <div className="section-title">
+                <h2><Image className="p-2" src="/images/News.png" width={50} height={50} alt="" /> Breaking News</h2>
+                {/* <h5 style={{ color: "#ff661f", fontWeight: 600, opacity: "80%" }}>Rekomendasi Wisata Alam di Bali</h5> */}
+              </div>
+              <Carousel cols={4} rows={1} gap={20} loop>
+                {news.map((item, index) => (
+                  <Carousel.Item key={index}>
+                    <div className="row" key={item.rowid}>
+                      <div className="col-lg-3 col-sm-3 ">
+                        <div className="single-tech-news-box rounded-3" style={{ width: "16rem" }}>
+                          <a href="">
+                            {item.image == 0 ? (<>
+                              <img src="/images/tech-news/tech-news-1.jpg" alt="image" />
+                            </>) : (<>
+                              <img src={item.image} style={{ width: "450px", height: "350px" }} />
                             </>)}
-                        </a>
-                      {/* <img className="card-img-top" src="/images/tech-news/tech-news-1.jpg" alt="Card image cap" /> */}
-                       <div className="tech-news-content">
-                        <h3>
-                          {/* <a href="../DetailNews/rowid">
-                           {item.judul}
-                          </a> */}
-                          <Link href={'/News/' + item.rowid} key={item.rowid}>
-                            {item.judul_news}
-                          </Link>
-                        
-                        </h3>
-                        <p>{item.sub_judul}</p>
-                        <p>{moment(item.ctddate).format("DD MMMM, YYYY ")}</p>
-                      </div>
+                          </a>
+                          {/* <img className="card-img-top" src="/images/tech-news/tech-news-1.jpg" alt="Card image cap" /> */}
+                          <div className="tech-news-content">
+                            <h3>
+                              <Link href={`/News/DetailNews?id=${item.rowid}`} key={item.rowid}>
+                                {item.judul_news}
+                              </Link>
+
+                            </h3>
+                            <p>{item.sub_judul}</p>
+                            <p>{moment(item.ctddate).format("DD MMMM, YYYY ")}</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                    </Carousel.Item>
-                  ))}
-                </Carousel>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
             </div>
-            </div>
-            </section>
-            {/* End Default News Area */}
-        </BaseLayouts>
+          </div>
+        </section>
+        {/* End Default News Area */}
+      </BaseLayouts>
     </>
   )
 }
