a1e097182e829733f14ec6293e749df5044da90d
RK380IB <rakhachips2@gmail.com>
2022-11-17 14:03:13 +0700

    updt kalender event

--- pages/News/KalenderEvent.js
+++ pages/News/KalenderEvent.js
@@ -51,9 +51,9 @@ export default function KalenderEvent() {
                 <div className="section-title"> 
                     <h2>Upcoming Events</h2> 
                 </div>
-                <Slide cols={3} rows={1} gap={20} loop>
+                <Carousel cols={4} rows={1} gap={8} loop>
                       {event.map((item, index) => (
-                        <Slide.Item key={index}>
+                        <Carousel.Item key={index}>
                           <div className="row" key={item.rowid}>
                             <div className='col-lg-4'>
                               <div className="single-main-news">
@@ -70,9 +70,9 @@ export default function KalenderEvent() {
                               </div>
                             </div>
                           </div>
-                        </Slide.Item>
+                        </Carousel.Item>
                       ))}
-                      </Slide>
+                      </Carousel>
                 </div>
             </section>
         {/* <!-- End Main News Slider Area -->
@@ -81,7 +81,7 @@ export default function KalenderEvent() {
         <section className="default-news-area ptb-30">
             <div className="container">
               {event.map((item, index) => (
-                  <div className="row events mb-20">
+                  <div className="row events mb-20" key={index}>
                     <div className="col-lg-9 ">
                         <div className="single-culture-news">
                             <div className="row align-items-center">
