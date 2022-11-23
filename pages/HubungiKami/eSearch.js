1db38d5c2c2e7f8f205d33afa4fe2227997ababc
RK380IB <rakhachips2@gmail.com>
2022-11-23 14:10:25 +0700

    updt e-search

---
+++ pages/HubungiKami/eSearch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BaseLayouts } from '../../components/MainCode/MainImport'
+
+export default function eSearch() {
+  return (
+    <>
+        <BaseLayouts>
+        
+        </BaseLayouts>
+    </>
+  )
+}
