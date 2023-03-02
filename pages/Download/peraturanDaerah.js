ee8f075a94f4702d55a23e5b82af00ca0857c97a
RK380 <rakhachips2@gmail.com>
2023-03-02 12:00:45 +0700

    updt perda

--- pages/Download/peraturanDaerah.js
+++ pages/Download/peraturanDaerah.js
@@ -5,8 +5,9 @@ export default function PeraturanDaerah() {
   return (
     <>
       <BaseLayouts>
-        <iframe src="https://jdih.baliprov.go.id/produk-hukum/peraturan-perundang-undangan/perda" width="90%" height="400" name="iframe" />
+        <p className='py-2 px-2 text-2xl'>Peraturan Daerah</p>
         
+        <iframe src="https://jdih.baliprov.go.id/produk-hukum/peraturan-perundang-undangan/perda" width="90%" height="400" name="iframe" />
       </BaseLayouts>
     </>
   )
